ebdcee8c20a345dd8db2e9600612b9bea5774914
Anhinla <116889127+Anhinla@users.noreply.github.com>
2025-05-17 15:16:04 +0700

    fix logo path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
 
   return (
     <div className="p-4 bg-white fixed w-full z-20">
-        <img src="/public/geekup.svg" className="w-25" alt="GeekUp logo"/>
+        <img src="/geekup.svg" className="w-25" alt="GeekUp logo"/>
     </div>
   )
 }
